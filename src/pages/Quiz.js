a2de93972cf1ae7ef3bcee3234a4b5e691b747ab
vhivestate <garcia.valeria001@gmail.com>
2023-04-25 16:52:48 -0600

    question cards

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,9 +1,148 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Quiz = () => {
   return (
     <div>
-      <h1>QUIZ</h1>
+    <div className="w-64 carousel rounded-box">
+        <div id="slide1" className="carousel-item relative w-full">
+        <div className="card w-96 bg-neutral text-neutral-content">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Question 1</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Accept</button>
+                <button className="btn btn-ghost">Deny</button>
+                </div>
+            </div>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide8" className="btn btn-circle">❮</a> 
+            <a href="#slide2" className="btn btn-circle">❯</a>
+            </div>
+        </div> 
+
+        <div id="slide2" className="carousel-item relative w-full">
+        <div className="card w-96 bg-neutral text-neutral-content">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Question 2</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Accept</button>
+                <button className="btn btn-ghost">Deny</button>
+                </div>
+            </div>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+            </div>
+        </div> 
+
+        <div id="slide3" className="carousel-item relative w-full">
+        <div className="card w-96 bg-neutral text-neutral-content">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Question 3</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Accept</button>
+                <button className="btn btn-ghost">Deny</button>
+                </div>
+            </div>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide4" className="btn btn-circle">❯</a>
+            </div>
+        </div> 
+
+        <div id="slide4" className="carousel-item relative w-full">
+        <div className="card w-96 bg-neutral text-neutral-content">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Question 4</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Accept</button>
+                <button className="btn btn-ghost">Deny</button>
+                </div>
+            </div>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a> 
+            <a href="#slide5" className="btn btn-circle">❯</a>
+            </div>
+        </div> 
+
+        <div id="slide5" className="carousel-item relative w-full">
+        <div className="card w-96 bg-neutral text-neutral-content">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Question 5</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Accept</button>
+                <button className="btn btn-ghost">Deny</button>
+                </div>
+            </div>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a> 
+            <a href="#slide6" className="btn btn-circle">❯</a>
+            </div>
+        </div> 
+
+        <div id="slide6" className="carousel-item relative w-full">
+        <div className="card w-96 bg-neutral text-neutral-content">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Question 6</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Accept</button>
+                <button className="btn btn-ghost">Deny</button>
+                </div>
+            </div>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide5" className="btn btn-circle">❮</a> 
+            <a href="#slide7" className="btn btn-circle">❯</a>
+            </div>
+        </div> 
+
+        <div id="slide7" className="carousel-item relative w-full">
+        <div className="card w-96 bg-neutral text-neutral-content">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Question 7</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Accept</button>
+                <button className="btn btn-ghost">Deny</button>
+                </div>
+            </div>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide6" className="btn btn-circle">❮</a> 
+            <a href="#slide8" className="btn btn-circle">❯</a>
+            </div>
+        </div> 
+
+        <div id="slide8" className="carousel-item relative w-full">
+        <div className="card w-96 bg-neutral text-neutral-content">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Question 8</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Accept</button>
+                <button className="btn btn-ghost">Deny</button>
+                </div>
+            </div>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide7" className="btn btn-circle">❮</a> 
+            <a href="#slide1" className="btn btn-circle">❯</a>
+            </div>
+        </div> 
+
+
+        </div>
     </div>
   )
 }
