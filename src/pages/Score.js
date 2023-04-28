6d0817bde424a013414a667a792ecdc4684af52b
vhivestate <garcia.valeria001@gmail.com>
2023-04-27 22:54:07 -0600

    question section

--- src/pages/Score.js
+++ src/pages/Score.js
@@ -13,9 +13,100 @@ function ScoreKeeper() {
 
   return (
     <div>
-      <h1 className='flex flex-wrap text-5xl text-center'>Score: {score}</h1>
-      <button className='btn glass' onClick={incrementScore}>Increase Score</button>
-      <button className='btn glass' onClick={decrementScore}>Decrease Score</button>
+      <h1 className='flex flex-wrap text-5xl text-center justify-center'>Score: {score}</h1>
+      {/* <button className='btn glass' onClick={incrementScore}>Increase Score</button>
+      <button className='btn glass' onClick={decrementScore}>Decrease Score</button> */}
+
+      <div id='quizSection'>
+      <div className='flex justify-center pt-5'>
+        <div className="w-64 carousel rounded-box">
+            <div id="slide1" className="carousel-item relative w-full">
+                <div className="card w-96 bg-neutral text-neutral-content">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Question 1</h2>
+                        <p>???????</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary"  onClick={incrementScore}>True</button>
+                            <button className="btn btn-ghost" onClick={decrementScore}>False</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+
+            <div id="slide2" className="carousel-item relative w-full">
+                <div className="card w-96 bg-neutral text-neutral-content">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Question 2</h2>
+                        <p>We are using cookies for no reason.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">True</button>
+                            <button className="btn btn-ghost">False</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">❮</a> 
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+
+            <div id="slide3" className="carousel-item relative w-full">
+                <div className="card w-96 bg-neutral text-neutral-content">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Question 3</h2>
+                        <p>We are using cookies for no reason.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">True</button>
+                            <button className="btn btn-ghost">False</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❮</a> 
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+
+            <div id="slide4" className="carousel-item relative w-full">
+                <div className="card w-96 bg-neutral text-neutral-content">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Question 4</h2>
+                        <p>We are using cookies for no reason.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">True</button>
+                            <button className="btn btn-ghost">False</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a> 
+                    <a href="#slide5" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+
+            <div id="slide5" className="carousel-item relative w-full">
+                <div className="card w-96 bg-neutral text-neutral-content">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Question 5</h2>
+                        <p>We are using cookies for no reason.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">True</button>
+                            <button className="btn btn-ghost">False</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a> 
+                </div>
+            </div> 
+        </div>
+        </div>
+      </div>
+      {/* end of quiz section */}
+      
     </div>
   );
 }
