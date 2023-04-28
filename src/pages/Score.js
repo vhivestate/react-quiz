1f2b9790fdefe07ab48fff6854aeb653a6e538c4
vhivestate <garcia.valeria001@gmail.com>
2023-04-27 22:01:37 -0600

    use the useState hook to create a score state variable that starts at 0. We then define two functions, incrementScore and decrementScore, that update the score state variable by adding or subtracting 1.
    In the return statement, we use JSX to display the current score and two buttons that call the incrementScore and decrementScore functions when clicked. Finally, we export the ScoreKeeper component as the default export.

--- src/pages/Score.js
+++ src/pages/Score.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Score = () => {
+function ScoreKeeper() {
+  const [score, setScore] = useState(0);
+
+  const incrementScore = () => {
+    setScore(score + 1);
+  };
+
+  const decrementScore = () => {
+    setScore(score - 1);
+  };
+
   return (
     <div>
-      
+      <h1>Score: {score}</h1>
+      <button onClick={incrementScore}>Increase Score</button>
+      <button onClick={decrementScore}>Decrease Score</button>
     </div>
-  )
+  );
 }
 
-export default Score
+export default ScoreKeeper;
